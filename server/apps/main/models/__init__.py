e2179633e3c8eed32372d746614bad53f98c103c
Pavel Korchagin <xoste49@gmail.com>
2023-03-15 00:13:13 +0300

    edit models

--- server/apps/main/models/__init__.py
+++ server/apps/main/models/__init__.py
@@ -1,6 +1,11 @@
+from server.apps.main.models.Address import Address
+from server.apps.main.models.Tariff import Tariff
+from server.apps.main.models.CashInflow import CashInflow
+from server.apps.main.models.Contract import Contract
+
 __all__ = [
     'Address',
-    'Contract',
     'Tariff',
     'CashInflow',
+    'Contract',
 ]
